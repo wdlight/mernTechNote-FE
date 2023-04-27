89526e01c9fb0a9a1cbc1725af1bf43deba6accf
wdlight <wdlight@naver.com>
2023-04-27 15:13:58 +0900

    [BugFix] EditNote.jsx import usersApiSlice  from UserApi~~

--- src/features/notes/EditNote.jsx
+++ src/features/notes/EditNote.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { selectNoteById } from "./notesApiSlice"
-import { selectAllUsers } from "../users/UsersApiSlice"
+import { selectAllUsers } from "../users/usersApiSlice"
 import EditNoteForm from "./EditNoteForm"
 
 
