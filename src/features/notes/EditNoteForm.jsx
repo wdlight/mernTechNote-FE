bcadb16354770b174c0cfdbedff44a8ae8f2b388
wdlight <wdlight@naver.com>
2023-04-27 15:19:28 +0900

    [BugFix] EditNote.jsx import usersApiSlice  from UserApi~~ NewNote.jsjx

--- src/features/notes/EditNoteForm.jsx
+++ src/features/notes/EditNoteForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { useUpdateNoteMutation, useDeleteNoteMutation } from './NotesApiSlice'
+import { useUpdateNoteMutation, useDeleteNoteMutation } from './notesApiSlice'
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSave, faTrashCan } from '@fortawesome/free-solid-svg-icons'
--- src/features/notes/NewNote.jsx
+++ src/features/notes/NewNote.jsx
@@ -1,16 +1,16 @@
-import { useSelector } from "react-redux"
-import { selectAllUsers } from "../users/UsersApiSlice"
-import NewNoteForm from "./NewNoteForm"
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersApiSlice";
+import NewNoteForm from "./NewNoteForm";
 
 const NewNote = () => {
-  const users = useSelector(selectAllUsers)
-  
-  // user가 없는 경우에 대해서 
-  if ( !users?.length ) return <p>Not Currently Aailable.</p>
-  
-  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
+  const users = useSelector(selectAllUsers);
 
-  return content
-}
+  // user가 없는 경우에 대해서
+  if (!users?.length) return <p>Not Currently Aailable.</p>;
 
-export default NewNote
+  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>;
+
+  return content;
+};
+
+export default NewNote;
